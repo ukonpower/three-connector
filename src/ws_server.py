1d8186d75a1550cf759e908909399421d4abfe2d
ukonpower <ukon.power@icloud.com>
2022-02-27 23:17:12 +0900

    wip

--- src/ws_server.py
+++ src/ws_server.py
@@ -2,6 +2,7 @@ import threading
 import json
 import inspect
 
+import logging
 from websocket_server import WebsocketServer
 
 clients = []
@@ -14,21 +15,24 @@ class WS:
         self.server = None
     
     def new_client(self, client, server):
-        print('open')
         clients.append(client)
 
     def client_left(self, client, server):
-        print('close')
+        l = len(clients)
+        for i in range(l):
+            index = l - i - 1
+            cli = clients[index]
+            if(cli["id"] == client["id"]):
+                del clients[index]
 
     def message_received(self, client, server, message):
         print('receive')
     
     def start_server(self, host, port):
-        
         if self.server:
             return False
         
-        self.server = WebsocketServer(host, port)
+        self.server = WebsocketServer(host, port, loglevel=logging.INFO)
         self.server.set_fn_new_client(self.new_client)
         self.server.set_fn_client_left(self.client_left)
         self.server.set_fn_message_received(self.message_received) 
@@ -41,7 +45,7 @@ class WS:
             return False
 
         # shutdown server
-        self.server.shutdown_gracefully()
+        self.server.shutdown_abruptly()
         self.server = None
 
         # clear clients
